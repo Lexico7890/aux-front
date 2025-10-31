48c0c4625395fa8b161f533ce9285537bf30708a
Oscar Casas <oscaralexander2626@gmail.com>
2025-10-31 14:22:24 -0500

    solucion renderizado del menu

--- src/components/InventoryPageWrapper.tsx
+++ src/components/InventoryPageWrapper.tsx
@@ -1,27 +1,24 @@
 // src/components/InventoryPageWrapper.tsx
 import React from 'react';
 import createPageWrapper from '@/lib/createPageWrapper';
-import Navigation from './Navigation';
 import InventoryForm from './InventoryForm';
 import InventoryTable from './InventoryTable';
 
 function InventoryPage() {
   return (
-    <Navigation>
-      <main className="max-w-7xl mx-auto px-4 py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          {/* Formulario */}
-          <div className="lg:col-span-1">
-            <InventoryForm />
-          </div>
-
-          {/* Tabla */}
-          <div className="lg:col-span-2">
-            <InventoryTable />
-          </div>
+    <main className="max-w-7xl mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        {/* Formulario */}
+        <div className="lg:col-span-1">
+          <InventoryForm />
         </div>
-      </main>
-    </Navigation>
+
+        {/* Tabla */}
+        <div className="lg:col-span-2">
+          <InventoryTable />
+        </div>
+      </div>
+    </main>
   );
 }
 
--- src/components/MovementsPageWrapper.tsx
+++ src/components/MovementsPageWrapper.tsx
@@ -1,16 +1,13 @@
 // src/components/MovementsPageWrapper.tsx
 import React from 'react';
 import createPageWrapper from '@/lib/createPageWrapper';
-import Navigation from './Navigation';
 import MovementCoworkers from './MovementCoworkers';
 
 function MovementsPage() {
   return (
-    <Navigation>
-      <main className="mx-auto sm:px-4 py-2 dark:bg-dark-700">
-        <MovementCoworkers />
-      </main>
-    </Navigation>
+    <main className="mx-auto sm:px-4 py-2 dark:bg-dark-700">
+      <MovementCoworkers />
+    </main>
   );
 }
 
